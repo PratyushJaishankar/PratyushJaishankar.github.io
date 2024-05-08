eb6d77cc142c3631ef1632f019a178ec1cc47fd4
Pratyush Jaishankar <pratjai97@gmail.com>
2024-05-09 00:44:58 +0530

    Added Deploy Script

--- app/(components)/Aboutme/AboutMe.jsx
+++ app/(components)/Aboutme/AboutMe.jsx
@@ -1,3 +1,5 @@
+import Image from 'next/image';
+
 const AboutMe = () => {
   return (
     <div className="bg-black text-white py-8 px-4 md:px-8">
@@ -6,18 +8,18 @@ const AboutMe = () => {
         <div className="grid grid-cols-1 md:grid-cols-2 gap-8 items-center">
           {/* First Part */}
           <div>
-            <img src="/aboutme.jpeg" alt="Your Photo" className="rounded-full w-48 h-48 mx-auto mb-4" />
+            <Image src="/aboutme.jpeg" alt="Your Photo" width={192} height={192} className="rounded-full w-48 h-48 mx-auto mb-4" />
             <p className="text-lg mb-4">
-              Hello! My name is [Your Name] and I'm a [Your Profession] based in [Your Location]. I have a passion for [Your Interests or Skills] and have been working in the field for [Years of Experience] years.
+              Hello! My name is [Your Name] and I&apos;m a [Your Profession] based in [Your Location]. I have a passion for [Your Interests or Skills] and have been working in the field for [Years of Experience] years.
             </p>
             <p className="text-lg mb-4">
-              In my career, I've had the opportunity to [Highlight one or two key accomplishments or projects]. My strengths lie in [Your Strengths or Skills].
+              In my career, I&apos;ve had the opportunity to [Highlight one or two key accomplishments or projects]. My strengths lie in [Your Strengths or Skills].
             </p>
           </div>
           {/* Second Part */}
           <div>
             <p className="text-lg mb-4">
-              A third-year student pursuing a degree in Electronics and Communication, I possess a solid understanding of technology. Despite my background, I've developed strong programming skills and gained practical experience through projects. With problem-solving abilities and effective communication skills, I am ready to transition into software engineering. I am committed to contributing positively and advancing in this field.
+              A third-year student pursuing a degree in Electronics and Communication, I possess a solid understanding of technology. Despite my background, I&apos;ve developed strong programming skills and gained practical experience through projects. With problem-solving abilities and effective communication skills, I am ready to transition into software engineering. I am committed to contributing positively and advancing in this field.
             </p>
           </div>
         </div>
