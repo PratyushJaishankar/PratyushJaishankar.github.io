d7403ccbd33400c60c182935ef77433fe4c4134a
Pratyush Jaishankar <pratjai97@gmail.com>
2024-06-01 12:01:16 +0530

    Hero Section Update

--- app/(components)/HeroSection/HeroSection.jsx
+++ app/(components)/HeroSection/HeroSection.jsx
@@ -6,8 +6,8 @@ const HeroSection = () => {
   return (
     <div className="flex items-center justify-center h-screen bg-black text-white">
       <div className="text-center">
-        <h1 className="text-4xl font-bold mb-4 transition-opacity delay-500 opacity-100">Welcome to My Website</h1>
-        <p className="text-lg mb-8 transition-opacity delay-1000 opacity-100">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        <h1 className="text-4xl font-bold mb-4 transition-opacity delay-500 opacity-100">Embark on a New Journey</h1>
+        <p className="text-lg mb-8 transition-opacity delay-1000 opacity-100">Passionate About Cloud Computing, Evolving in DevOps</p>
         <button
           className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded transition-opacity delay-1500 opacity-100"
         //   onClick={handleDownloadClick}
